483ff9eaadfff223ae1489e309a4b9413c0d0ae5
ChrisCPH <christoffer221199@hotmail.com>
2022-05-09 12:26:56 +0200

    Opdateret EER-diagram

--- Carport/src/main/java/dat/carport/model/entities/ServiceEntities/Materials.java
+++ Carport/src/main/java/dat/carport/model/entities/ServiceEntities/Materials.java
@@ -1,4 +1,50 @@
 package dat.carport.model.entities.ServiceEntities;
 
+import java.util.Objects;
+
 public class Materials {
+    int id;
+    String description;
+
+    public Materials(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Materials{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Materials materials = (Materials) o;
+        return id == materials.id && description.equals(materials.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
 }
