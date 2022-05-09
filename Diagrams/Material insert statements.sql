14423db19f987749b25a414cf4198da6cd095ee7
ChrisCPH <christoffer221199@hotmail.com>
2022-05-09 12:31:26 +0200

    Mappers

---
+++ Diagrams/Material insert statements.sql
@@ -0,0 +1,6 @@
+INSERT INTO carport.materials VALUES 
+('1','Trykimp. brædt length: 360mm, height: 200mm, width: 25mm, understærnbredder til for og bag ende'),
+('2','Trykimp. brædt length: 540mm, height: 200mm, width: 25mm, understærnbredder til siderne'),
+('3','Trykimp. brædt length: 360mm, height: 125mm, width: 25mm, overstærnbredder til forenden'),
+('4','Trykimp. brædt length: 540mm, height: 125mm, width: 25mm, overstærnbredder til siderne'),
+('5','Lægte ubh. length: 420mm, height: 73mm, width: 38mm, til z på bagside af dør'),
