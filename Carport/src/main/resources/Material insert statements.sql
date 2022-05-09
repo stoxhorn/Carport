cd7abd40ef9e550dde91bb62e9ef98ec012a525a
ChrisCPH <christoffer221199@hotmail.com>
2022-05-09 12:53:26 +0200

    Insert statements

---
+++ Carport/src/main/resources/Material insert statements.sql
@@ -0,0 +1,16 @@
+INSERT INTO carport.materials VALUES 
+('1','Trykimp. brædt length: 360mm, height: 200mm, width: 25mm, understærnbredder til for og bag ende'),
+('2','Trykimp. brædt length: 540mm, height: 200mm, width: 25mm, understærnbredder til siderne'),
+('3','Trykimp. brædt length: 360mm, height: 125mm, width: 25mm, overstærnbredder til forenden'),
+('4','Trykimp. brædt length: 540mm, height: 125mm, width: 25mm, overstærnbredder til siderne'),
+('5','Lægte ubh. length: 420mm, height: 73mm, width: 38mm, til z på bagside af dør'),
+('6','Reglar ub. length: 270mm, height: 95mm, width: 45mm, løsholter til skur gavle'),
+('7','Reglar ub. length: 240mm, height: 95mm, width: 45mm, løsholter til skur sider'),
+('8','Spærtræ ubh. length: 600mm, height: 195mm, width: 45mm, Remme i sider, sadles ned i stolper'),
+('9','Spærtræ ubh. length: 480mm, height: 195mm, width: 45mm, Remme i sider, sadles ned i stolper (skur del, deles)'),
+('10','Spærtræ ubh. length: 600mm, height: 195mm, width: 45mm, Remme i sider, spær, monteres på rem'),
+('10','Trykimp. stolpe length: 300mm, height: 97mm, width: 97mm, Remme i sider, stolper nedgraves 90 cm. i jord'),
+('11','Trykimp. stolpe length: 210mm, height: 100mm, width: 97mm, Remme i sider, stolper nedgraves 90 cm. i jord'),
+
+
+
