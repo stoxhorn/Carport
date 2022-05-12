bc62c426fa6880dafec414923b1067d44a3f31ca
madpacket <mads-keini@hotmail.com>
2022-05-12 16:32:42 +0200

    asd

--- Carport/src/main/java/dat/carport/control/CRUDCustomerRequestCommand.java
+++ Carport/src/main/java/dat/carport/control/CRUDCustomerRequestCommand.java
@@ -8,6 +8,37 @@ import javax.servlet.http.HttpServletResponse;
 public class CRUDCustomerRequestCommand extends Command{
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
-        return null;
+        String crudType = request.getParameter("crud");
+        String customerEmail = request.getParameter("customerEmail");
+/*
+        if(){
+        }
+else if(){
+
+        }
+    else if(){
+
+        }
+        else if(){
+
+            }
+  */      return null;
+        }
+
+
+    private void readCustomerRequest(String customerEmail){
+
+    }
+
+    private void createCustomerRequest(String customerEmail){
+
+    }
+
+    private void updateCustomerRequest(String customerEmail){
+
+    }
+
+    private void deleteCustomerRequest(String customerEmail){
+
     }
 }
