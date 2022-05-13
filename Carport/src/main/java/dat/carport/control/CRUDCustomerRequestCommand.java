dc923e5242692704be66f06b2fde24762eb490c3
madpacket <mads-keini@hotmail.com>
2022-05-13 16:05:56 +0200

    CRUDMAterialsCommand Actually done now

--- Carport/src/main/java/dat/carport/control/CRUDCustomerRequestCommand.java
+++ Carport/src/main/java/dat/carport/control/CRUDCustomerRequestCommand.java
@@ -18,8 +18,10 @@ public class CRUDCustomerRequestCommand extends Command{
     public CRUDCustomerRequestCommand(){this.connectionPool = ApplicationStart.getConnectionPool();}
 
     /**
-     *
      * There is a lack of optimization when interacting with database, goes through everything
+     * Everything was build around using customerEmail as the ID to search for,
+     * as it cannot be expected to always have the ID
+     * crud can be create, read, update, delete. ANything else will not register
      * @param request
      * @param response
      * @return
@@ -29,49 +31,50 @@ public class CRUDCustomerRequestCommand extends Command{
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
         String crudType = request.getParameter("crud");
         String customerEmail = request.getParameter("customerEmail");
-        if(crudType.equals("create")){
-            CustomerRequestData crData = new CustomerRequestData(
-                    request.getParameter("carportWidth"),
-                    request.getParameter("carportLength"),
-                    request.getParameter("roofType"),
-                    request.getParameter("roofMaterial"),
-                    request.getParameter("roofSlope"),
-                    request.getParameter("shedWidth"),
-                    request.getParameter("shedLength"));
-            try{
-                CRUDCustomerRequestService.createCustomerRequest(customerEmail, crData, this.connectionPool);
-            } catch (DatabaseException e) {
-                throw new RuntimeException(e);
+        switch (crudType) {
+            case "create": {
+                CustomerRequestData crData = new CustomerRequestData(
+                        request.getParameter("carportWidth"),
+                        request.getParameter("carportLength"),
+                        request.getParameter("roofType"),
+                        request.getParameter("roofMaterial"),
+                        request.getParameter("roofSlope"),
+                        request.getParameter("shedWidth"),
+                        request.getParameter("shedLength"));
+                try {
+                    CRUDCustomerRequestService.createCustomerRequest(customerEmail, crData, this.connectionPool);
+                } catch (DatabaseException e) {
+                    throw new RuntimeException(e);
+                }
+                return request.getParameter("next");
             }
-            return request.getParameter("next");
-        }
-        else if(crudType.equals("read")){
-            CustomerRequest cr = CRUDCustomerRequestService.readCustomerRequest(customerEmail, this.connectionPool);
-            request.setAttribute("readCustomerRequest", cr);
-        }
-        else if(crudType.equals("update")){
-            CustomerRequestData crData = new CustomerRequestData(
-                    request.getParameter("carportWidth"),
-                    request.getParameter("carportLength"),
-                    request.getParameter("roofType"),
-                    request.getParameter("roofMaterial"),
-                    request.getParameter("roofSlope"),
-                    request.getParameter("shedWidth"),
-                    request.getParameter("shedLength"));
-            try{
-                CRUDCustomerRequestService.updateCustomerRequest(customerEmail, crData, this.connectionPool);
-            } catch (DatabaseException e) {
-                throw new RuntimeException(e);
+            case "read":
+                CustomerRequest cr = CRUDCustomerRequestService.readCustomerRequest(customerEmail, this.connectionPool);
+                request.setAttribute("readCustomerRequest", cr);
+                break;
+            case "update": {
+                CustomerRequestData crData = new CustomerRequestData(
+                        request.getParameter("carportWidth"),
+                        request.getParameter("carportLength"),
+                        request.getParameter("roofType"),
+                        request.getParameter("roofMaterial"),
+                        request.getParameter("roofSlope"),
+                        request.getParameter("shedWidth"),
+                        request.getParameter("shedLength"));
+                try {
+                    CRUDCustomerRequestService.updateCustomerRequest(customerEmail, crData, this.connectionPool);
+                } catch (DatabaseException e) {
+                    throw new RuntimeException(e);
+                }
+                return request.getParameter("next");
             }
-            return request.getParameter("next");
-        }
-        else if(crudType.equals("delete")){
-            try{
-                CRUDCustomerRequestService.deleteCustomerRequest(customerEmail, this.connectionPool);
-            } catch (DatabaseException e) {
-                throw new RuntimeException(e);
-            }
-            return request.getParameter("next");
+            case "delete":
+                try {
+                    CRUDCustomerRequestService.deleteCustomerRequest(customerEmail, this.connectionPool);
+                } catch (DatabaseException e) {
+                    throw new RuntimeException(e);
+                }
+                return request.getParameter("next");
         }
         return request.getParameter("next");
         }
